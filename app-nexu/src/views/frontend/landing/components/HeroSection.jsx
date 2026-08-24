eac0e888a6c843e5cd1b31c3aa507829357f0748
Jonathan Medina <jonathan603015@gmail.com>
2026-08-24 12:28:01 -0700

    chore: limpieza de imports y selectores css obsoletos en landing

--- app-nexu/src/views/frontend/landing/components/HeroSection.jsx
+++ app-nexu/src/views/frontend/landing/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { IconShield, IconArrowRight } from '../../../../components/icons/Icons'
 
 function HeroSection({
